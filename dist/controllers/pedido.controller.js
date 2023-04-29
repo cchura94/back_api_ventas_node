f419b4aaaa43ffc281cddf2d52470c6343bab818
cchura94 <cchura.cpc@gmail.com>
2023-04-28 23:57:12 -0400

    COnsulta pedidos

--- dist/controllers/pedido.controller.js
+++ dist/controllers/pedido.controller.js
@@ -13,7 +13,7 @@ var _default = {
   listar: async (req, res) => {
     try {
       const pedidos = await _index.default.Pedido.findAll({
-        include: [_index.default.Cliente, _index.default.Producto]
+        // include: [models.Cliente, models.Producto]
       });
       return res.status(200).json(pedidos);
     } catch (err) {
